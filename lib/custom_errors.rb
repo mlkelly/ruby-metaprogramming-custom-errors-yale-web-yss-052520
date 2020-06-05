4133138c769b26750df7c09ff801b6b37cb46a74
mlkelly <maurak87@gmail.com>
2020-06-05 00:00:29 -0400

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,12 +7,34 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    # this will raise the PartnerError class below when arguments are given incorrectly
+    if person.class != Person
+      # rescue code inside truthy statment
+      # rescue method creates instance of PartnerError class & calls message on that instance
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    else
+      person.partner = self
+    end
+  end
+
+  # puting PartnerError class inside Person class so can use with #get_married?
+  class PartnerError < StandardError
+    # outputs customer error message when error occurs
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
   end
 
 end
 
+
 beyonce = Person.new("Beyonce")
+# we're trying to pass a string, not an instance of the person class
+# will produce undefined method error
 beyonce.get_married("Jay-Z")
 puts beyonce.name
 
